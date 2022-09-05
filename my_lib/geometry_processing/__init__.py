03cf67fd0e8e64c3127cd0f98ef5f8fd5492315e
Kentechx <skaidi@chohotech.com>
2022-09-05 16:14:51 +0800

    update

--- my_lib/geometry_processing/__init__.py
+++ my_lib/geometry_processing/__init__.py
@@ -1,4 +1,4 @@
-from .common import (laplacian_smooth, smooth_boundary, smooth_line, laplacian_smooth_selected,
+from .common import (laplacian_smooth, smooth_boundary, smooth_line, laplacian_smooth_selected, solve_laplacian_smooth,
                      deform_arap_igl, grid_triangulation
                      )
 from .copy_left import CGAL_Helper, PymeshlabHelper, PygmshHelper
