28395c2cd981008801fb8ab61d013fc94763c6e0
Kentechx <kd_shen@163.com>
2022-09-07 03:38:46 +0800

    update

--- my_lib/image.py
+++ my_lib/image.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from skimage import img_as_ubyte
 from PIL import Image, ImageOps
+from typing import List, Dict, Any
 
 
 class ImageManipulator:
@@ -17,11 +18,11 @@ class ImageManipulator:
         _, thresh_im = cv2.threshold(mask, 0.5, 255, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(thresh_im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         img_contour = np.zeros_like(mask)
-        cv2.drawContours(img_contour, contours, -1, color, thickness)       # -1 indicates all contours are drawn
+        cv2.drawContours(img_contour, contours, -1, color, thickness)  # -1 indicates all contours are drawn
         return img_contour
 
     @staticmethod
-    def crop_with_mask(img:np.ndarray, mask:np.ndarray, margin=(50, 50)):
+    def crop_with_mask(img: np.ndarray, mask: np.ndarray, margin=(50, 50)):
         """
         :param img:
         :param mask: with shape (h, w, 3)
@@ -33,28 +34,29 @@ class ImageManipulator:
         else:
             x, y, w, h = cv2.boundingRect(mask)
         wm, hm = margin
-        left, top, right , bottom = max(x-wm, 0), max(y-hm, 0), min(x+w+wm, img.shape[1]), min(y+h+hm, img.shape[0])
+        left, top, right, bottom = max(x - wm, 0), max(y - hm, 0), min(x + w + wm, img.shape[1]), min(y + h + hm,
+                                                                                                      img.shape[0])
         return img[top:bottom, left:right], mask[top:bottom, left:right]
 
     @staticmethod
-    def resize_and_pad(img:np.ndarray, mask:np.ndarray, target_res=512):
+    def resize_and_pad(img: np.ndarray, mask: np.ndarray, target_res=512):
         # this will keep the aspect ratio of the content
         # resize
         ratio = target_res / max(img.shape)
-        new_size = (int(ratio*img.shape[1]), int(ratio*img.shape[0]))       # (w, h)!
+        new_size = (int(ratio * img.shape[1]), int(ratio * img.shape[0]))  # (w, h)!
         img = cv2.resize(img, new_size)
         mask = cv2.resize(mask, new_size)
 
         # pad
         delta_w = target_res - new_size[0]
         delta_h = target_res - new_size[1]
-        padding = (delta_w//2, delta_h//2, delta_w-delta_w//2, delta_h-delta_h//2)
+        padding = (delta_w // 2, delta_h // 2, delta_w - delta_w // 2, delta_h - delta_h // 2)
         img, mask = Image.fromarray(img), Image.fromarray(mask)
         img, mask = ImageOps.expand(img, padding), ImageOps.expand(mask, padding)
         return np.array(img), np.array(mask)
 
     @staticmethod
-    def remove_small_components(image:np.ndarray, thresh=10):
+    def remove_small_components(image: np.ndarray, thresh=10):
         """
         :param image:  (h, w), in range (0, c), where `c` is the max class label
         :param thresh:
@@ -70,7 +72,7 @@ class ImageManipulator:
         return ret_image
 
     @staticmethod
-    def blend(img1:np.ndarray, alphas, img2):
+    def blend(img1: np.ndarray, alphas, img2):
         """
         :param img1: of shape (h, w, 3)
         :param alphas: of shape (h, w) or (h, w, 3)
@@ -78,9 +80,9 @@ class ImageManipulator:
         :return:
         """
         alphas = alphas.astype('f4')
-        if len(alphas.shape)==2:
-            alphas = alphas[...,None]
-        return (img1 * alphas + img2 * (1-alphas)).astype('u1')
+        if len(alphas.shape) == 2:
+            alphas = alphas[..., None]
+        return (img1 * alphas + img2 * (1 - alphas)).astype('u1')
 
     @staticmethod
     def visualize_switch(img1, img2):
@@ -99,6 +101,17 @@ class ImageManipulator:
                     img = img1
             else:
                 break
+
+    @staticmethod
+    def visualize_images(imgs: List[np.ndarray], cols=1):
+        # stack images
+        _imgs = []
+        for i in range(0, len(imgs), cols):
+            _imgs.append(np.hstack(imgs[i:i + cols]))
+        _imgs = np.concatenate(_imgs, axis=0)
+        cv2.imshow('', _imgs)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
     @staticmethod
     def visualize(img):
