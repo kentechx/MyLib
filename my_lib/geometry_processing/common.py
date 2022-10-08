d63887b65477164c9ac84dea97c24596c017e200
Kentechx <skaidi@chohotech.com>
2022-10-08 17:58:26 +0800

    fix changing to csr

--- my_lib/geometry_processing/common.py
+++ my_lib/geometry_processing/common.py
@@ -198,8 +198,7 @@ def uniform_laplacian_matrix(fs: np.ndarray, nv: int, normalize: bool = False, k
 
 def cot_laplacian_matrix(vs: np.ndarray, fs: np.ndarray, normalize: bool = False,
                          k: int = 1) -> scipy.sparse.csr_matrix:
-    L = scipy.sparse.csr_matrix((vs.shape[0], vs.shape[0]))
-    L[:] = -igl.cotmatrix(vs, fs)
+    L = -igl.cotmatrix(vs, fs).asformat('csr')
     if np.any(np.isnan(L.data)):
         L.data[np.isnan(L.data)] = 0.
     if np.any(np.isinf(L.data)):
