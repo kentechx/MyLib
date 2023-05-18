01be243d9aee6c49621e0fd661894104c084bd38
Kentechx <skaidi@chohotech.com>
2023-05-18 19:56:20 +0800

    update

--- my_lib/file_helper.py
+++ my_lib/file_helper.py
@@ -1,14 +1,15 @@
 import os, shutil
 from typing import List
 
+
 class FileHelper:
 
     @staticmethod
-    def overwrite_files(src_fps:List[str], dst_fps:List[str]):
-        src_fn_fp_dict = {os.path.split(fp)[-1]:fp for fp in src_fps}
-        dst_fn_fp_dict = {os.path.split(fp)[-1]:fp for fp in dst_fps}
-
-        for fn, dst_fp in dst_fn_fp_dict.items():
-            src_fp = src_fn_fp_dict[fn]
-            shutil.copy(src_fp, dst_fp)
-            print('Overwrite file', dst_fp)
+    def calc_md5(filename):
+        """Calculate the MD5 hash for a file."""
+        import hashlib
+        md5_hash = hashlib.md5()
+        with open(filename, "rb") as file:
+            for chunk in iter(lambda: file.read(4096), b""):
+                md5_hash.update(chunk)
+        return md5_hash.hexdigest()
